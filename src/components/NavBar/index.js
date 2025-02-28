ca10c3d15860b615b10ef2c7c27ac6dd4fed79b5
guiggac <guilhermecarmosp@hotmail.com>
2025-02-28 16:34:33 -0300

    Alterações na NavBar - Desabilitados botoes que nao funcionam

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom"; // Importação do Link
+import { Link, useLocation } from "react-router-dom"; // Adiciona useLocation
 import linkedInDefault from "../../assets/imgs/LinkedIn-Default.svg";
 import linkedInHover from "../../assets/imgs/LinkedIn-Hoover.svg";
 import menuIcon from "../../assets/imgs/Menu.svg";
@@ -9,6 +9,12 @@ import "./styles.css";
 const NavBar = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [hover, setHover] = useState(false);
+    const location = useLocation(); // Pega a localização atual da página
+
+    // Função para verificar se a página está ativa
+    const getNavItemClass = (path) => {
+        return location.pathname === path ? 'active' : ''; // Aplica a classe active se a página for a mesma
+    };
 
     return (
         <>
@@ -28,10 +34,42 @@ const NavBar = () => {
 
                 <nav className="nav-bar-mobile">
                     <ul>
-                        <li><Link to="/" onClick={() => setIsOpen(false)}>Home</Link></li> {/* Alterado para Link */}
-                        <li><Link to="/artes" onClick={() => setIsOpen(false)}>Artes</Link></li> {/* Alterado para Link */}
-                        <li><Link to="/sobre" onClick={() => setIsOpen(false)}>Sobre</Link></li> {/* Alterado para Link */}
-                        <li><Link to="/contato" onClick={() => setIsOpen(false)}>Contato</Link></li> {/* Alterado para Link */}
+                        <li>
+                            <Link 
+                                to="/" 
+                                className={getNavItemClass('/')} 
+                                onClick={() => setIsOpen(false)}
+                            >
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link 
+                                to="/artes" 
+                                className={`desabilitado ${getNavItemClass('/artes')}`} // Adiciona a classe desabilitado
+                                onClick={() => setIsOpen(false)}
+                            >
+                                Artes
+                            </Link>
+                        </li>
+                        <li>
+                            <Link 
+                                to="/sobre" 
+                                className={`desabilitado ${getNavItemClass('/sobre')}`} // Adiciona a classe desabilitado
+                                onClick={() => setIsOpen(false)}
+                            >
+                                Sobre
+                            </Link>
+                        </li>
+                        <li>
+                            <Link 
+                                to="/contato" 
+                                className={getNavItemClass('/contato')} 
+                                onClick={() => setIsOpen(false)}
+                            >
+                                Contato
+                            </Link>
+                        </li>
                     </ul>
                 </nav>
 
@@ -50,10 +88,38 @@ const NavBar = () => {
             {/* Menu desktop normal */}
             <nav className="nav-bar-desktop">
                 <ul>
-                    <li><Link to="/">Home</Link></li> {/* Alterado para Link */}
-                    <li><Link to="/artes">Artes</Link></li> {/* Alterado para Link */}
-                    <li><Link to="/sobre">Sobre</Link></li> {/* Alterado para Link */}
-                    <li><Link to="/contato">Contato</Link></li> {/* Alterado para Link */}
+                    <li>
+                        <Link 
+                            to="/" 
+                            className={getNavItemClass('/')} 
+                        >
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link 
+                            to="/artes" 
+                            className={`desabilitado ${getNavItemClass('/artes')}`} // Adiciona a classe desabilitado
+                        >
+                            Artes
+                        </Link>
+                    </li>
+                    <li>
+                        <Link 
+                            to="/sobre" 
+                            className={`desabilitado ${getNavItemClass('/sobre')}`} // Adiciona a classe desabilitado
+                        >
+                            Sobre
+                        </Link>
+                    </li>
+                    <li>
+                        <Link 
+                            to="/contato" 
+                            className={getNavItemClass('/contato')} 
+                        >
+                            Contato
+                        </Link>
+                    </li>
                 </ul>
                 <a 
                     href="https://www.linkedin.com/in/gcarmo/" 
