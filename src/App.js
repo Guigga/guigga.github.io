b1b648293a5143b7e352aab242248a5da1f02ae4
guiggac <guilhermecarmosp@hotmail.com>
2025-02-28 15:47:24 -0300

    Corrige navegação com HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom"; // Alterado de BrowserRouter para HashRouter
 import AppRoutes from "./routes";
 
 function App() {
