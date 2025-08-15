45bf928337b693b16016d1bd76d590ca7a1272dd
guigga <guilhermecarmosp@hotmail.com>
2025-08-15 11:51:18 -0300

    testes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -18,7 +18,7 @@ export default function Home() {
             
             {/* Títulos das sessões*/}
             <TituloSessao
-                titulo="Meus Trabalhooooooooooooooooooos"
+                titulo="Meus Trabalhoooooooooooooooooooos"
                 subtitulo="Casos selecionados"
             />
 
