5c112647dc23cd1776126feb7aff05309d598374
guigga <guilhermecarmosp@hotmail.com>
2025-08-15 10:34:25 -0300

    Nova atualização do site

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -18,7 +18,7 @@ export default function Home() {
             
             {/* Títulos das sessões*/}
             <TituloSessao
-                titulo="Meus Trabalaaaaaaaaaaaaaahos"
+                titulo="Meus Trabalhos"
                 subtitulo="Casos selecionados"
             />
 
